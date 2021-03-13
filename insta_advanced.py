a5ed3aa3e578e82010442fe3b9e5d498d393f784
Shiva-slbs <me_shivam@pm.me>
2021-03-13 09:58:02 +0530

    Update insta_advanced.py

--- insta_advanced.py
+++ insta_advanced.py
@@ -1,5 +1,11 @@
+##########################
+###pip install instabot###
+##########################
 from instabot import Bot
 bot=Bot()
 bot.login(username=input("Enter your username, Email or Phone Number : "), password=input("Enter your password : "))
 bot.upload_photo(input("Enter the link of photo you want to upload : "),caption=input("Enter caotion : "))
 bot.logout
+#################################################################
+#### Coded by Shivam kumar (https://www.github.com/Shiva-slbs)###
+#################################################################
